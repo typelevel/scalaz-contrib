bef32b58a9a0bc76808f9cae839d16335802f0d8
Jed Wesley-Smith <jed@atlassian.com>
2012-12-08 13:06:24 +1100

    clean ups, no need for elaborate names, formatting etc.

--- validation-ext/main/scala/validation/Validation.scala
+++ validation-ext/main/scala/validation/Validation.scala
@@ -14,34 +14,41 @@ trait Length[T] {
 }
 
 object Length {
-  implicit def seqLength = new Length[Seq[_]] {def length(seq: Seq[_]) = seq.length}
-  implicit def optionLength = new Length[Option[_]] {def length(o: Option[_]) = if (o.isDefined) 1 else 0}
+  implicit val seqLength = 
+    new Length[Seq[_]] {
+      def length(seq: Seq[_]) = seq.length
+    }
+
+  implicit val optionLength = 
+    new Length[Option[_]] {
+      def length(o: Option[_]) = if (o.isDefined) 1 else 0
+    }
 }
 
 trait Validation {
 
   type Valid[F, T] = T => ValidationNEL[F, T]
 
-  def max[T: Ordering, F](max: T, failure: F): Valid[F, T] = 
-    valid(_ <= max, failure)
+  def max[T: Ordering, F](max: T, f: F): Valid[F, T] = 
+    valid(_ <= max, f)
 
-  def min[T: Ordering, F](minT: T, failure: F): Valid[F, T] =
-    valid(_ >= minT, failure)
+  def min[T: Ordering, F](min: T, f: F): Valid[F, T] =
+    valid(_ >= min, f)
 
-  def equalO[T: Ordering, F](equalT: T, failure: F): Valid[F, T] = 
-    valid(_ >= equalT, failure)
+  def equalO[T: Ordering, F](equal: T, f: F): Valid[F, T] = 
+    valid(_ >= equal, f)
 
-  def equalE[T: Equal, F](equalT: T, failure: F) : Valid[F, T] =     
-    valid(_ === equalT, failure)
+  def equalE[T: Equal, F](equal: T, f: F) : Valid[F, T] =     
+    valid(_ === equal, f)
 
-  def maxSize[T: Length, F](max: Int, failure: F): Valid[F, T] =
-    valid(implicitly[Length[T]].length(_) <= max, failure) 
+  def maxSize[T: Length, F](max: Int, f: F): Valid[F, T] =
+    valid(implicitly[Length[T]].length(_) <= max, f) 
 
-  def minSize[T: Length, F](max: Int, failure: F): Valid[F, T] =
-    valid(implicitly[Length[T]].length(_) >= max, failure)
+  def minSize[T: Length, F](max: Int, f: F): Valid[F, T] =
+    valid(implicitly[Length[T]].length(_) >= max, f)
 
-  def notEmpty[T: Length,F](failure: F): Valid[F, T] = 
-    valid(implicitly[Length[T]].length(_) === 0, failure)
+  def notEmpty[T: Length,F](f: F): Valid[F, T] = 
+    valid(implicitly[Length[T]].length(_) === 0, f)
 
   private def valid[T, F](p: T => Boolean, f: F): Valid[F, T] = t => if(p(t)) t.success else f.failNel
 }
