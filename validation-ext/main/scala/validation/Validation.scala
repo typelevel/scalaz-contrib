fec84f1b9cf21d09320a8373e06e8a285f3f1734
Travis <travis.stevens@gaiam.com>
2012-12-07 18:06:18 -0700

    a few style changes.  Use context bounds and type definition

--- validation-ext/main/scala/validation/Validation.scala
+++ validation-ext/main/scala/validation/Validation.scala
@@ -3,52 +3,12 @@ package validation
 
 import scalaz._
 import Scalaz._
+import scala.Ordering
 
 /**
- * User: travis.stevens@gaiam.com
+ * User: OleTraveler
  * Date: 12/7/12
  */
-trait Validation {
-
-  def max[T, F](maxT: T, failure: F)(implicit ord: scala.Ordering[T]): (T) => ValidationNEL[F, T] = t => {
-    if (t <= maxT) t.success
-    else failure.failNel
-  }
-
-  def min[T,F](minT: T, failure: F)(implicit ord: scala.Ordering[T]): (T) => ValidationNEL[F,T] = t => {
-    if (t >= minT) t.success
-    else failure.failNel
-  }
-
-  def equalO[T,F](equalT: T, failure: F)(implicit ord: scala.Ordering[T]): (T) => ValidationNEL[F,T] = t => {
-    if (t >= equalT) t.success
-    else failure.failNel
-  }
-
-  def equalE[T,F](equalT: T, failure: F)(implicit eq: Equal[T]) : (T) => ValidationNEL[F,T] = t => {
-    if (t === equalT) t.success
-    else failure.failNel
-  }
-
-  def maxSize[T, F](max: Int, failure: F)(implicit l: Length[T]): (T) => ValidationNEL[F,T] = t => {
-    if (l.length(t) <= max) t.success
-    else failure.failNel
-  }
-
-  def minSize[T, F](max: Int, failure: F)(implicit l: Length[T]): (T) => ValidationNEL[F,T] = t => {
-    if (l.length(t) >= max) t.success
-    else failure.failNel
-  }
-
-  def notEmpty[T,F](failure: F)(implicit l: Length[T]): (T) => ValidationNEL[F,T] = t => {
-    if (l.length(t) === 0) t.success
-    else failure.failNel
-  }
-
-
-}
-
-
 trait Length[T] {
   def length(t: T): Int
 }
@@ -57,6 +17,51 @@ object Length {
   implicit def seqLength = new Length[Seq[_]] {def length(seq: Seq[_]) = seq.length}
   implicit def optionLength = new Length[Option[_]] {def length(o: Option[_]) = if (o.isDefined) 1 else 0}
 }
+
+
+
+trait Validation {
+
+  type Valid[T, F] = T => ValidationNEL[F, T]
+
+  def max[T:Ordering, F](maxT: T, failure: F): Valid[T, F] = t => {
+    if (t <= maxT) t.success
+    else failure.failNel
+  }
+
+  def min[T:Ordering,F](minT: T, failure: F): Valid[T, F] = t => {
+    if (t >= minT) t.success
+    else failure.failNel
+  }
+
+  def equalO[T:Ordering,F](equalT: T, failure: F): Valid[T, F] = t => {
+    if (t >= equalT) t.success
+    else failure.failNel
+  }
+
+  def equalE[T:Equal,F](equalT: T, failure: F) : Valid[T, F] = t => {
+    if (t === equalT) t.success
+    else failure.failNel
+  }
+
+  def maxSize[T:Length, F](max: Int, failure: F): (T) => ValidationNEL[F,T] = t => {
+    if (implicitly[Length[T]].length(t) <= max) t.success
+    else failure.failNel
+  }
+
+  def minSize[T:Length, F](max: Int, failure: F): (T) => ValidationNEL[F,T] = t => {
+    if (implicitly[Length[T]].length(t) >= max) t.success
+    else failure.failNel
+  }
+
+  def notEmpty[T: Length,F](failure: F): (T) => ValidationNEL[F,T] = t => {
+    if (implicitly[Length[T]].length(t) === 0) t.success
+    else failure.failNel
+  }
+
+
+}
+
 
 
 
