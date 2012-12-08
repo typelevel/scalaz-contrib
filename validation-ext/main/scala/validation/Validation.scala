ff8330c78f9cda0076e85be1a91373d5d8343d0e
Jed Wesley-Smith <jed@atlassian.com>
2012-12-08 12:52:22 +1100

    consistent type param order

--- validation-ext/main/scala/validation/Validation.scala
+++ validation-ext/main/scala/validation/Validation.scala
@@ -22,39 +22,39 @@ object Length {
 
 trait Validation {
 
-  type Valid[T, F] = T => ValidationNEL[F, T]
+  type Valid[F, T] = T => ValidationNEL[F, T]
 
-  def max[T:Ordering, F](maxT: T, failure: F): Valid[T, F] = t => {
+  def max[T:Ordering, F](maxT: T, failure: F): Valid[F, T] = t => {
     if (t <= maxT) t.success
     else failure.failNel
   }
 
-  def min[T:Ordering,F](minT: T, failure: F): Valid[T, F] = t => {
+  def min[T:Ordering,F](minT: T, failure: F): Valid[F, T] = t => {
     if (t >= minT) t.success
     else failure.failNel
   }
 
-  def equalO[T:Ordering,F](equalT: T, failure: F): Valid[T, F] = t => {
+  def equalO[T:Ordering,F](equalT: T, failure: F): Valid[F, T] = t => {
     if (t >= equalT) t.success
     else failure.failNel
   }
 
-  def equalE[T:Equal,F](equalT: T, failure: F) : Valid[T, F] = t => {
+  def equalE[T:Equal,F](equalT: T, failure: F) : Valid[F, T] = t => {
     if (t === equalT) t.success
     else failure.failNel
   }
 
-  def maxSize[T:Length, F](max: Int, failure: F): Valid[T, F] = t => {
+  def maxSize[T:Length, F](max: Int, failure: F): Valid[F, T] = t => {
     if (implicitly[Length[T]].length(t) <= max) t.success
     else failure.failNel
   }
 
-  def minSize[T:Length, F](max: Int, failure: F): Valid[T, F] = t => {
+  def minSize[T:Length, F](max: Int, failure: F): Valid[F, T] = t => {
     if (implicitly[Length[T]].length(t) >= max) t.success
     else failure.failNel
   }
 
-  def notEmpty[T: Length,F](failure: F): Valid[T, F] = t => {
+  def notEmpty[T: Length,F](failure: F): Valid[F, T] = t => {
     if (implicitly[Length[T]].length(t) === 0) t.success
     else failure.failNel
   }
