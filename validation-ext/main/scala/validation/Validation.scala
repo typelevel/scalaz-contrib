bee09dd4af99eb45bd4da0d7930fbbf206c1ca17
Travis <travis.stevens@gaiam.com>
2012-12-09 19:34:15 -0700

    Use scalaz.Length instead of homegrown Length.
    also added test for all functions

--- validation-ext/main/scala/validation/Validation.scala
+++ validation-ext/main/scala/validation/Validation.scala
@@ -5,30 +5,20 @@ import scalaz._
 import Scalaz._
 import scala.Ordering
 
+
 /**
- * User: OleTraveler
- * Date: 12/7/12
+ * Some basic validation functions.
  */
-trait Length[T] {
-  def length(t: T): Int
-}
-
-object Length {
-  implicit val seqLength = 
-    new Length[Seq[_]] {
-      def length(seq: Seq[_]) = seq.length
-    }
-
-  implicit val optionLength = 
-    new Length[Option[_]] {
-      def length(o: Option[_]) = if (o.isDefined) 1 else 0
-    }
-}
-
 trait Validation {
 
   type Valid[F, T] = T => ValidationNEL[F, T]
 
+  /**
+   *
+   * @param max The max, can be any type that has an Ordering
+   * @param f The return object if there is a failure.
+   * @return {(t: T) => if (notValid) {Failure(NonEmptyList(t)) else {(t: T) => Success(t)} .
+   */
   def max[T: Ordering, F](max: T, f: F): Valid[F, T] = 
     valid(_ <= max, f)
 
@@ -38,16 +28,16 @@ trait Validation {
   def equalO[T: Ordering, F](equal: T, f: F): Valid[F, T] = 
     valid(_ == equal, f)
 
-  def equalE[T: Equal, F](equal: T, f: F) : Valid[F, T] =     
+  def equal[T: Equal, F](equal: T, f: F) : Valid[F, T] =
     valid(_ === equal, f)
 
-  def maxSize[T: Length, F](max: Int, f: F): Valid[F, T] =
-    valid(implicitly[Length[T]].length(_) <= max, f) 
+  def minSize[V, T[_]: Length, F](min: Int, f: F): Valid[F, T[V]] =
+    valid(implicitly[Length[T]].length(_) >= min, f)
 
-  def minSize[T: Length, F](max: Int, f: F): Valid[F, T] =
-    valid(implicitly[Length[T]].length(_) >= max, f)
+  def maxSize[V, T[_]: Length, F](max: Int, f: F): Valid[F, T[V]] =
+    valid(implicitly[Length[T]].length(_) <= max, f)
 
-  def notEmpty[T: Length,F](f: F): Valid[F, T] = 
+  def notEmpty[V, T[_]: Length,F](f: F): Valid[F, T[V]] =
     valid(implicitly[Length[T]].length(_) === 0, f)
 
   private def valid[T, F](p: T => Boolean, f: F): Valid[F, T] = t => if(p(t)) t.success else f.failNel
