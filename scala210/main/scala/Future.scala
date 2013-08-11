c73783504dacce869344362c3e37e4c87e0ad44b
Lars Hupel <lars.hupel@mytum.de>
2013-08-11 23:35:46 +0200

    make Applicative[Future] behave like expected: parallel execution

--- scala210/main/scala/Future.scala
+++ scala210/main/scala/Future.scala
@@ -8,33 +8,15 @@ import scala.concurrent.duration.Duration
 
 trait FutureInstances1 {
 
-  private[scalaz] class FutureSemigroup[A](implicit m: Semigroup[A], executionContext: ExecutionContext) extends Semigroup[Future[A]] {
-    def append(f1: Future[A], f2: => Future[A]): Future[A] = for {
-      first <- f1
-      second <- f2
-    } yield m.append(first, second)
-  }
+  implicit def futureInstance(implicit ec: ExecutionContext): Monad[Future] with Cobind[Future] with Cojoin[Future] with Each[Future] = 
+    new FutureInstance
 
-  implicit def FutureSemigroup[A](implicit m: Semigroup[A], executionContext: ExecutionContext): Semigroup[Future[A]] = new FutureSemigroup[A]
+  implicit def futureSemigroup[A](implicit m: Semigroup[A], ec: ExecutionContext): Semigroup[Future[A]] =
+    Monoid.liftSemigroup[Future, A]
 
 }
 
 trait FutureInstances extends FutureInstances1 {
-
-  private[scalaz] class FutureInstance(implicit ec: ExecutionContext) extends Monad[Future] with Cobind[Future] with Cojoin[Future] with Each[Future] {
-
-    def point[A](a: => A): Future[A] = Future(a)
-    def bind[A, B](fa: Future[A])(f: A => Future[B]): Future[B] = fa flatMap f
-    override def map[A, B](fa: Future[A])(f: A => B): Future[B] = fa map f
-    def cobind[A, B](fa: Future[A])(f: Future[A] => B): Future[B] = Future(f(fa))
-    def cojoin[A](a: Future[A]): Future[Future[A]] = Future(a)
-    def each[A](fa: Future[A])(f: A => Unit) = fa foreach f
-
-  }
-
-  implicit def futureInstance(implicit executionContext: ExecutionContext): Monad[Future] with Cobind[Future] with Cojoin[Future] with Each[Future] = 
-    new FutureInstance
-
 
   /**
    * Requires explicit usage as the use of `Await.result`. Can throw an exception, which is inherently bad.
@@ -43,12 +25,26 @@ trait FutureInstances extends FutureInstances1 {
     def copoint[A](f: Future[A]): A = Await.result(f, duration)
   }
 
+  implicit def futureMonoid[A](implicit g: Monoid[A], ec: ExecutionContext): Monoid[Future[A]] =
+    Monoid.liftMonoid[Future, A]
 
-  private[scalaz] class FutureMonoid[A](implicit m: Monoid[A], executionContext: ExecutionContext) extends FutureSemigroup[A] with Monoid[Future[A]] {
-    def zero: Future[A] = Future(m.zero)
+}
+
+private class FutureInstance(implicit ec: ExecutionContext) extends Monad[Future] with Cobind[Future] with Cojoin[Future] with Each[Future] {
+
+  def point[A](a: => A): Future[A] = Future(a)
+  def bind[A, B](fa: Future[A])(f: A => Future[B]): Future[B] = fa flatMap f
+  override def map[A, B](fa: Future[A])(f: A => B): Future[B] = fa map f
+  def cobind[A, B](fa: Future[A])(f: Future[A] => B): Future[B] = Future(f(fa))
+  def cojoin[A](a: Future[A]): Future[Future[A]] = Future(a)
+  def each[A](fa: Future[A])(f: A => Unit) = fa foreach f
+
+  // override for actual parallel execution
+  override def ap[A, B](_fa: => Future[A])(_fab: => Future[A => B]) = {
+    val fa = _fa
+    val fab = _fab
+    for { a <- fa; ab <- fab } yield ab(a)
   }
-
-  implicit def FutureMonoid[A](implicit g: Monoid[A], executionContext: ExecutionContext): Monoid[Future[A]] = new FutureMonoid[A]
 
 }
 
